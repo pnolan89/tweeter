aab7978f335d8d5fc2fbf14353659e2e39c720bf
pnolan89 <petenol@gmail.com>
2019-01-23 20:10:33 +0000

    add loadTweets function to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,4 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
+// Given a tweet JSON object, constructs a DOM node of it
 function createTweetElement(tweet) {
     let $tweet = $('<article>').addClass("tweet");
     let header = $('<header>');
@@ -57,15 +11,16 @@ function createTweetElement(tweet) {
       $('<span>').addClass("handle").text(tweet.user.handle)
     );
     icons.append(
-      $('<span>').text('♥'),
-      $('<span>').text('↻'),
-      $('<span>').text('⚑')
+      $('<i>').addClass('fas fa-heart'),
+      $('<i>').addClass('fas fa-retweet'),
+      $('<i>').addClass('fas fa-flag')
     );
     footer.append($('<span>').addClass("timestamp").text("10 days ago"), icons);
     $tweet.append(header, content, footer);
    return $tweet[0].outerHTML;
 }
 
+// Loops through array of tweet objects, passes each one to createTweetElement, and appends the rendered result to index.html
 function renderTweets(tweets) {
   for (let i in tweets) {
      let render = createTweetElement(tweets[i]);
@@ -74,8 +29,34 @@ function renderTweets(tweets) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
+  // Get tweet JSONs from the server
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    })
+    .then(function(data) {
+      renderTweets(data);
+    });
+  }
+  loadTweets();
 });
 
+// When 'Compose Tweet' form is submitted, post  data to server
+$(function () {
+  let $form = $('.new-tweet');
+  $form.on('submit', function(event) {
+    event.preventDefault();
+    let serialize = $(this).find('form').serialize();
+    $.ajax({
+      method: 'POST',
+      url: '/tweets',
+      data: serialize
+    })
+    .then(function () {
+      console.log('Success');
+    });
+  });
+});
 
 
