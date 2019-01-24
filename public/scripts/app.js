dd9e2e684b2ebf5e46b1a56b2e2b67c684dca4a8
pnolan89 <petenol@gmail.com>
2019-01-24 01:44:25 +0000

    change hover styling for composeBtn

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,31 @@
 // Given a tweet JSON object, constructs a DOM node of it
 function createTweetElement(tweet) {
-    let $tweet = $('<article>').addClass("tweet");
-    let header = $('<header>');
-    let content = $('<p>').addClass("tweetContent").text(tweet.content.text);
-    let footer = $('<footer>');
-    let icons = $('<div>').addClass("icons");
-    header.append(
-      $('<img>').attr("src", tweet.user.avatars.small),
-      $('<span>').addClass("userName").text(tweet.user.name),
-      $('<span>').addClass("handle").text(tweet.user.handle)
-    );
-    icons.append(
-      $('<i>').addClass('fas fa-heart'),
-      $('<i>').addClass('fas fa-retweet'),
-      $('<i>').addClass('fas fa-flag')
-    );
-    footer.append($(`<span data-livestamp="${(tweet.created_at)/1000}">`));
-    $tweet.append(header, content, footer);
-   return $tweet[0].outerHTML;
+  let $tweet = $('<article>').addClass("tweet");
+  let header = $('<header>');
+  let content = $('<p>').addClass("tweetContent").text(tweet.content.text);
+  let footer = $('<footer>');
+  let icons = $('<div>').addClass("icons");
+  header.append(
+    $('<img>').attr("src", tweet.user.avatars.small),
+    $('<span>').addClass("userName").text(tweet.user.name),
+    $('<span>').addClass("handle").text(tweet.user.handle)
+  );
+  icons.append(
+    $('<i>').addClass('fas fa-heart'),
+    $('<i>').addClass('fas fa-retweet'),
+    $('<i>').addClass('fas fa-flag')
+  );
+  footer.append($(`<span data-livestamp="${(tweet.created_at)/1000}">`));
+  $tweet.append(header, content, footer);
+  return $tweet[0].outerHTML;
 }
 
 // Loops through array of tweet objects, passes each one to createTweetElement, and appends the rendered result to index.html
 function renderTweets(tweets) {
   $('#tweets-container').empty();
   for (let i = (tweets.length - 1); i >= 0; i--) {
-     let render = createTweetElement(tweets[i]);
-     $('#tweets-container').append(render);
+    let render = createTweetElement(tweets[i]);
+    $('#tweets-container').append(render);
   }
 }
 
@@ -52,11 +52,15 @@ $(function() {
     let text = $(this).find('textarea').val();
     // Check if tweet is empty or exceeding the character limit
     if (text === null || text === "") {
+      $('#errorMsg').slideUp(function() {
+        $('#errorMsg').text("Error: You didn't enter any text!");
+      });
       $('#errorMsg').slideDown();
-      $('#errorMsg').text("Error: You didn't enter any text!");
     } else if (text.length > 140) {
+      $('#errorMsg').slideUp(function() {
+        $('#errorMsg').text("Error: Character limit exceeded!");
+      });
       $('#errorMsg').slideDown();
-      $('#errorMsg').text("Error: Character limit exceeded!");
     // Otherwise, post data to server
     } else {
       $('#errorMsg').slideUp();
@@ -79,14 +83,11 @@ $(function() {
     if (composeHidden === true) {
       $(".new-tweet").slideDown();
       $(".new-tweet").find('textarea').focus();
-      $(this).addClass('clicked');
-      let classes = $(this).attr("class");
-      console.log(classes);
       composeHidden = false;
     } else {
       $(".new-tweet").slideUp();
-      $(this).removeClass('clicked');
       composeHidden = true;
     }
   });
 });
+
